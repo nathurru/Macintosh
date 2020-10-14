fa45bb17a164289ab09da67366472ef68f74e99d
nathurru <nathurru@neln.net>
2020-10-15 01:05:28 +0900

    update

--- scripts/1_LaunchDaemons.sh
+++ scripts/1_LaunchDaemons.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-SYSTEM_DAEMON_DIR=/Library/LaunchDaemons
-SYSTEM_AGENT_DIR=/Library/LaunchAgents
+GLOBAL_DAEMON_DIR=/Library/LaunchDaemons
+GLOBAL_AGENT_DIR=/Library/LaunchAgents
 USER_AGENT_DIR=~/Library/LaunchAgents
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 LAUNCHD_DIR=$(cd ${SCRIPT_DIR}/../launchds; pwd)
 
-for file in `\find ${LAUNCHD_DIR}/systemDaemons -maxdepth 1 -name '*.plist'`; do
+for file in `\find ${LAUNCHD_DIR}/globalDaemons -maxdepth 1 -name '*.plist'`; do
 
-    DST_FILE=${SYSTEM_DAEMON_DIR}/$(basename ${file})
+    DST_FILE=${GLOBAL_DAEMON_DIR}/$(basename ${file})
     printf "Setting ${DST_FILE}..."
     if [ -e  ${DST_FILE} ]; then
         printf "\e[33mEXISTS\e[m\n"
     else
-        sudo cp ${file} ${SYSTEM_DAEMON_DIR}
+        sudo cp ${file} ${GLOBAL_DAEMON_DIR}
         sudo chmod 644 ${DST_FILE}
         sudo chown root:wheel ${DST_FILE}
+        sudo launchctl load ${DST_FILE}
         printf "\e[32mDONE\e[m\n"
     fi
 done
 
-for file in `\find ${LAUNCHD_DIR}/systemAgents -maxdepth 1 -name '*.plist'`; do
-    DST_FILE=${SYSTEM_AGENT_DIR}/$(basename ${file})
+for file in `\find ${LAUNCHD_DIR}/globalAgents -maxdepth 1 -name '*.plist'`; do
+    DST_FILE=${GLOBAL_AGENT_DIR}/$(basename ${file})
     printf "Setting ${DST_FILE}..."
     if [ -e  ${DST_FILE} ]; then
         printf "\e[33mEXISTS\e[m\n"
     else
-        sudo cp ${file} ${SYSTEM_AGENT_DIR}
+        sudo cp ${file} ${GLOBAL_AGENT_DIR}
         sudo chmod 644 ${DST_FILE}
         sudo chown root:wheel ${DST_FILE}
+        sudo launchctl load ${DST_FILE}
         printf "\e[32mDONE\e[m\n"
     fi
 done
@@ -39,8 +41,9 @@ for file in `\find ${LAUNCHD_DIR}/userAgents -maxdepth 1 -name '*.plist'`; do
     if [ -e  ${DST_FILE} ]; then
         printf "\e[33mEXISTS\e[m\n"
     else
-        sudo cp ${file} ${USER_AGENT_DIR}
-        sudo chmod 644 ${DST_FILE}
+        cp ${file} ${USER_AGENT_DIR}
+        chmod 644 ${DST_FILE}
+        launchctl load ${DST_FILE}
         printf "\e[32mDONE\e[m\n"
     fi
 done
